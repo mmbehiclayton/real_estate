405345937de257c438ca136ed1c9f87d2082fa1d
Clayton Mmbehi <mmbehiclayton@gmail.com>
2022-11-09 02:06:28 +0300

    update settings

--- real_estate_app/real_estate_app/settings.py
+++ real_estate_app/real_estate_app/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -116,6 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = 'static/'
 MEDIA_URL ='media/'
 MEDIA_ROOT = 'media'
